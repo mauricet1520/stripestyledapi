7f5395a80e35610fb8bd4c2ce657aaec66dd215e
Maurice Thomas <mauricet1520@gmail.com>
2021-01-03 09:12:34 -0500

    Adds a grouping for Time Slots

--- src/main/kotlin/com/styledbylovee/stripestyledapi/manager/SetmoreManager.kt
+++ src/main/kotlin/com/styledbylovee/stripestyledapi/manager/SetmoreManager.kt
@@ -3,7 +3,6 @@ package com.styledbylovee.stripestyledapi.manager
 import com.styledbylovee.stripestyledapi.model.setmore.appointment.CreateAppointmentRequest
 import com.styledbylovee.stripestyledapi.model.setmore.appointment.CreateAppointmentResponse
 import com.styledbylovee.stripestyledapi.model.setmore.appointment.StyledCustomerAppointmentRequest
-import com.styledbylovee.stripestyledapi.model.setmore.appointment.UpdateAppointmentRequest
 import com.styledbylovee.stripestyledapi.model.setmore.customer.CreateCustomerRequest
 import com.styledbylovee.stripestyledapi.model.setmore.customer.CreateCustomerResponse
 import com.styledbylovee.stripestyledapi.model.setmore.services.FetchAllServicesResponse
@@ -16,6 +15,9 @@ import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
 import org.springframework.web.client.HttpClientErrorException
+import java.util.*
+import kotlin.collections.ArrayList
+import kotlin.collections.HashSet
 
 @Component
 class SetmoreManager(@Autowired val setmoreService: SetmoreService, @Autowired val fireBaseService: FireBaseService) {
@@ -112,7 +114,8 @@ class SetmoreManager(@Autowired val setmoreService: SetmoreService, @Autowired v
             val slots = mapOfTimeSlots[it.key]
 
             if (slots != null) {
-                listOfStaffTimeSlots.add(StaffTimeSlots(it, slots))
+                val staffTimeSlots = StaffTimeSlots(it, slots)
+                listOfStaffTimeSlots.add(checkAvailableTimeSlots(staffTimeSlots))
             }
 
         }
@@ -148,5 +151,94 @@ class SetmoreManager(@Autowired val setmoreService: SetmoreService, @Autowired v
         createAppointment(styledCustomerAppointmentRequest.createAppointmentRequest!!)
     } catch (h: HttpClientErrorException) {
         throw h
+    }
+
+    fun checkAvailableTimeSlots(staffTimeSlot: StaffTimeSlots): StaffTimeSlots {
+
+            val sevenNineTimeSlots = HashSet<String>()
+            val eightTenTimeSlots =  HashSet<String>()
+            val nineElevenTimeSlots =  HashSet<String>()
+            val tenTwelveTimeSlots =  HashSet<String>()
+            val elevenThirteenTimeSlots =  HashSet<String>()
+            val twelveFourteenTimeSlots =  HashSet<String>()
+            val thirteenFifteenTimeSlots =  HashSet<String>()
+            val fourteenSixteenTimeSlots =  HashSet<String>()
+
+            staffTimeSlot.slots.forEach {
+                when(it) {
+                    "07.00"-> sevenNineTimeSlots.add("07.00")
+                    "07.30"-> sevenNineTimeSlots.add("07.30")
+                    "08.00"-> sevenNineTimeSlots.add("08.00")
+                    "08.30"-> sevenNineTimeSlots.add("08.30")
+                    "09.00"-> sevenNineTimeSlots.add("09.00")
+                }
+
+                when(it) {
+                    "08.00"-> eightTenTimeSlots.add("08.00")
+                    "08.30"-> eightTenTimeSlots.add("08.30")
+                    "09.00"-> eightTenTimeSlots.add("09.00")
+                    "09.30"-> eightTenTimeSlots.add("09.30")
+                    "10.00"-> eightTenTimeSlots.add("10.00")
+                }
+
+                when(it) {
+                    "09.00"-> nineElevenTimeSlots.add("09.00")
+                    "09.30"-> nineElevenTimeSlots.add("09.30")
+                    "10.00"-> nineElevenTimeSlots.add("10.00")
+                    "10.30"-> nineElevenTimeSlots.add("10.30")
+                    "11.00"-> nineElevenTimeSlots.add("11.00")
+                }
+
+                when(it) {
+                    "10.00"-> tenTwelveTimeSlots.add("10.00")
+                    "10.30"-> tenTwelveTimeSlots.add("10.30")
+                    "11.00"-> tenTwelveTimeSlots.add("11.00")
+                    "11.30"-> tenTwelveTimeSlots.add("11.30")
+                    "12.00"-> tenTwelveTimeSlots.add("12.00")
+                }
+
+                when(it) {
+                    "11.00"-> elevenThirteenTimeSlots.add("11.00")
+                    "11.30"-> elevenThirteenTimeSlots.add("11.30")
+                    "12.00"-> elevenThirteenTimeSlots.add("12.00")
+                    "12.30"-> elevenThirteenTimeSlots.add("12.30")
+                    "13.00"-> elevenThirteenTimeSlots.add("13.00")
+                }
+
+                when(it) {
+                    "12.00"-> twelveFourteenTimeSlots.add("12.00")
+                    "12.30"-> twelveFourteenTimeSlots.add("12.30")
+                    "13.00"-> twelveFourteenTimeSlots.add("13.00")
+                    "13.30"-> twelveFourteenTimeSlots.add("13.30")
+                    "14.00"-> twelveFourteenTimeSlots.add("14.00")
+                }
+
+                when(it) {
+                    "13.00"-> thirteenFifteenTimeSlots.add("13.00")
+                    "13.30"-> thirteenFifteenTimeSlots.add("13.30")
+                    "14.00"-> thirteenFifteenTimeSlots.add("14.00")
+                    "14.30"-> thirteenFifteenTimeSlots.add("14.30")
+                    "15.00"-> thirteenFifteenTimeSlots.add("15.00")
+                }
+
+                when(it) {
+                    "14.00"-> fourteenSixteenTimeSlots.add("14.00")
+                    "14.30"-> fourteenSixteenTimeSlots.add("14.30")
+                    "15.00"-> fourteenSixteenTimeSlots.add("15.00")
+                    "15.30"-> fourteenSixteenTimeSlots.add("15.30")
+                    "16.00"-> fourteenSixteenTimeSlots.add("16.00")
+                }
+            }
+
+            if (sevenNineTimeSlots.size >= 5) staffTimeSlot.sevenToNineTimeSlotsAvailable = true
+            if (eightTenTimeSlots.size >= 5) staffTimeSlot.eightToTenTimeSlotsAvailable = true
+            if (nineElevenTimeSlots.size >= 5) staffTimeSlot.nineToElevenTimeSlotsAvailable = true
+            if (tenTwelveTimeSlots.size >= 5) staffTimeSlot.tenToTwelveTimeSlotsAvailable = true
+            if (elevenThirteenTimeSlots.size >= 5) staffTimeSlot.elevenToThirteenTimeSlotsAvailable = true
+            if (twelveFourteenTimeSlots.size >= 5) staffTimeSlot.twelveToFourteenTimeSlotsAvailable = true
+            if (thirteenFifteenTimeSlots.size >= 5) staffTimeSlot.thirteenToFifteenTimeSlotsAvailable = true
+            if (fourteenSixteenTimeSlots.size >= 5) staffTimeSlot.fourteenToSixteenTimeSlotsAvailable = true
+
+        return staffTimeSlot
     }
 }
--- src/main/kotlin/com/styledbylovee/stripestyledapi/model/setmore/stafftimeslots/StaffTimeSlots.kt
+++ src/main/kotlin/com/styledbylovee/stripestyledapi/model/setmore/stafftimeslots/StaffTimeSlots.kt
@@ -2,4 +2,14 @@ package com.styledbylovee.stripestyledapi.model.setmore.stafftimeslots
 
 import com.styledbylovee.stripestyledapi.model.setmore.staff.Staff
 
-data class StaffTimeSlots(val staff: Staff, val slots: List<String>)
+data class StaffTimeSlots(val staff: Staff,
+                          val slots: List<String>,
+                          var sevenToNineTimeSlotsAvailable: Boolean = false,
+                          var eightToTenTimeSlotsAvailable: Boolean = false,
+                          var nineToElevenTimeSlotsAvailable: Boolean= false,
+                          var tenToTwelveTimeSlotsAvailable: Boolean= false,
+                          var elevenToThirteenTimeSlotsAvailable: Boolean= false,
+                          var twelveToFourteenTimeSlotsAvailable: Boolean= false,
+                          var thirteenToFifteenTimeSlotsAvailable: Boolean= false,
+                          var fourteenToSixteenTimeSlotsAvailable: Boolean= false
+)
